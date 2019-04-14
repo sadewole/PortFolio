00f394b31b1ad2346afa3c1d5f6aafe0932ca8bd
Samuel Adewole <samueladewole15@gmail.com>
2019-04-14 01:38:05 +0100

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,6 +15,7 @@ function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
+        menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(items => items.classList.add('show'))
 
@@ -23,6 +24,7 @@ function toggleMenu() {
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
+        menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(items => items.classList.remove('show'))
 
